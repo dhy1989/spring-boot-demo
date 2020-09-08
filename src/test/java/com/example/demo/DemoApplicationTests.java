2d67d62f6ad67c38ae998673b2823b5f8d91aa78
dinghy <88888888>
2020-09-08 14:39:33 +0800

    添加redis配置

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import com.example.demo.common.RedisUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
+	@Autowired
+    private RedisUtils redisUtils;
 	@Test
 	void contextLoads() {
+		redisUtils.set("hello","world");
 	}
 
 }
